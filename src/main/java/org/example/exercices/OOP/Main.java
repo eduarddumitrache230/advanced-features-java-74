5a3764ef78fcd974a177c13b0ecb1e432b5b0eb6
eduarddumitrache230 <edydumitrache@yahoo.com>
2024-08-26 20:54:46 +0300

    Added exercise package for OOP theory (#1)
    * Added MapTheory + exercices OOP
    
    * Created parent+child class
    
    * Created parent+child class
    
    * Created parent+child class
    
    * Created parent+child class

---
+++ src/main/java/org/example/exercices/OOP/Main.java
@@ -0,0 +1,27 @@
+package org.example.exercices.OOP;
+/*
+ Create a multimedia player system where each multimedia item (audio, video) has its own methods to play and stop.
+ -clasa parinte MultimediaItem (instantiaza cele 2 metode generice play stop)
+ -clasa copil MultimediaAudio si MultimediaVideo
+
+    Implement abstraction by defining an abstract class MultimediaItem with abstract methods for playing and stopping.
+ -clasa parinte va fi abstracta
+ -metodele generice din clasa parinte vor fi abstracte
+
+    Create concrete classes Audio and Video that extend the MultimediaItem class and implement their specific methods
+    for playing and stopping.
+ -cream clasele copil  si MultimediaVideo
+ -suprascriem metodele play/stop din clasa parinte
+ */
+public class Main {
+    public static void main(String[] args) {
+        MultimediaAudio multimediaAudio = new MultimediaAudio("Audio Song");
+        MultimediaVideo multimediaVideo = new MultimediaVideo("Video movie");
+
+        multimediaAudio.play();
+        multimediaAudio.stop();
+
+        multimediaVideo.play();
+        multimediaVideo.stop();
+    }
+}
---
+++ src/main/java/org/example/exercices/OOP/MultimediaItem.java
@@ -0,0 +1,75 @@
+package org.example.exercices.OOP;
+
+/*
+    Clasa parinte(clasa de configuratie):
+    - poate fi abstracta deoarece nu ne intereseaza sa o utilizam pentru a instantia obiecte, pt acest scop vom avea
+    clasele copil care o mostenesc.
+    -atributele pot fi protected deoarece ne intereseaza ca ele sa poata fi vizibile in clasele copil fara a utiliza
+    getters/setters
+    -constructori neparametrizati sau parametrizati // getter/setter/toString
+    -metodele pot fi abstracte, deoarece daca mi cautam sa instantiam obiecte dupa clasa parinte,
+    nu va fi necesar sa stocam metode cu corp de implementare pentru ca nu vor fi apelate niciodata.
+ */
+public abstract class MultimediaItem {
+    protected String title;
+
+    public MultimediaItem(){
+
+    }
+    public MultimediaItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "MultimediaItem{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+
+    public abstract void play();
+
+    public abstract void stop();
+}
+
+class MultimediaAudio extends MultimediaItem {
+    public MultimediaAudio(String title) {
+        super(title);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("MultimediaAudio "+ title+ " plays");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("MultimediaAudio "+ title+ " stops");
+
+    }
+}
+
+class MultimediaVideo extends MultimediaItem {
+    @Override
+    public void play() {
+        System.out.println("MultimediaVideo "+ title+ " plays");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("MultimediaVideo "+ title+ " stops");
+    }
+
+    public MultimediaVideo(String title) {
+        super(title);
+    }
+}
+
