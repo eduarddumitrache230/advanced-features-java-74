0d84620830f96db563024a55c3fdff6c6192b8ea
Eduard <edydumitrache@yahoo.com>
2024-08-29 19:13:37 +0300

    Example of stream with sort and filter

--- src/main/java/org/example/streams/StreamsTheory.java
+++ src/main/java/org/example/streams/StreamsTheory.java
@@ -3,6 +3,7 @@ package org.example.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 /*
     Stream-urile se utilizeaza doar in contextul structurilor de date!!!!!!!!!
@@ -33,8 +34,47 @@ List<String> orderedNames = names
         .stream()
         .sorted()
         .toList();
+// List -> Stream ->Stream ->List
 
         System.out.println("Lista dezordonata: " + names);
         System.out.println("Lista ordonata: " + orderedNames);
+        System.out.println("------------------------------------------------------");
+
+        List<String> names2 = Arrays.asList("Alice","Bob","Anna","Beatrice","Joe", "Aty");
+
+        // Pas 1: Operatia de conversie a structurii de date la tipul de date generic pentru secvente -> Stream
+        Stream<String > streamOfNames = names2.stream();
+
+        // Pas 2: Operatiile intermediare (filter,map,distinct,sort, etc...)
+        Stream<String> filtredStreamOfNamesWith3Letters = streamOfNames.filter(it -> it.length()==3);
+        //putem avea oricate operatii intermediare vrem, toate avand ca tip de date Stream
+        //pentru ca toate prelucrarile se fac sub aceeasi tipologie
+
+        //Pas 3: Operatia terminala -> facem conversia de la Stream la tipul de date al rezultatului pe care ni-l dorim
+        List<String> listOfNamesWith3Letters = filtredStreamOfNamesWith3Letters.toList();
+        System.out.println("List with streams: " + listOfNamesWith3Letters);
+
+        //Echivalentul algoritmului in mod iterativ
+        //"Alice"-0,"Bobo"-1,"Anna"-2,"Beatrice"-3,"Joe"-4
+        //declaram o sublista ca si rezultat final al filtrarii
+        // parcurgem lista cu un FOR (0,n-1)
+        //vom avea o instructiune conditionala IF(it.lenght() ==3)
+        //daca e adevarat, stocam elementele in sublista
+        //afisam lista
+        List<String > result = new ArrayList<>();
+        for (String it: names2) {
+            if(it.length() == 3) {
+                result.add(it);
+            }
+        }
+        System.out.println("List iterativ: " + result);
+
+        //Echivalentul algoritmului in mod stream pipeline
+
+        List<String> finalListOfNames = names2
+                .stream()
+                .filter(it -> it.length() ==3)
+                .toList();
+        System.out.println("List with streams pipeline: " + finalListOfNames);
     }
 }
